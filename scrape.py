7ddd02f4cb67ea06e8c30ac2a22253f2c35676cb
JiaYouChen2003 <justin041510@gmail.com>
2023-12-08 01:19:22 +0100

    :bug:Now Chrome really run headless while scraping

--- scrape.py
+++ scrape.py
@@ -1,7 +1,8 @@
-from selenium import webdriver 
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.chrome.service import Service as ChromeService 
-from webdriver_manager.chrome import ChromeDriverManager 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 url_en_front = 'https://en.'
 
@@ -9,9 +10,11 @@ url = input('Enter the URL:')
 url_en_backstartnum = url.find('boardgamearena.com')
 url_en = url_en_front + url[url_en_backstartnum:]
 
-options = webdriver.ChromeOptions()
-options.headless = True
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+print('Loading recent website...')
 driver.get(url_en)
 
 card_board = driver.find_element(By.ID, 'player-boards')
