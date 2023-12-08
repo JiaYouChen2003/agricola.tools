da92906ce899ac5f468b0e63bfcf79c4249e910b
JiaYouChen2003 <justin041510@gmail.com>
2023-12-08 16:51:53 +0100

    :art:Move getValue and getIndex funcion into class SearchMachine

--- search.py
+++ search.py
@@ -2,38 +2,38 @@ import openpyxl
 import scrape
 import os
 
-def getValuesFromSheet(sheet):
-    arr = []
-    for row in sheet:
-        arr2 = []
-        for column in row:
-            arr2.append(str(column.value).strip())
-        arr.append(arr2)
-    return arr
-
-def getItemIndexByNameFromArray(itemName, arr, column4name, column4index):
-    for row in arr:
-        if row[column4name] == itemName:
-            return row[column4index]
-
 class SearchMachine():
     def __init__(self):
         PATH = os.path.abspath('./raw_asset/card_statistic/Jul_2023/agricola_statistic.xlsx')
         self.xlsx_name = PATH
         self.workbook_list = openpyxl.load_workbook(self.xlsx_name)
+
+    def getValuesFromSheet(sheet):
+        arr = []
+        for row in sheet:
+            arr2 = []
+            for column in row:
+                arr2.append(str(column.value).strip())
+            arr.append(arr2)
+        return arr
+
+    def getItemIndexByNameFromArray(itemName, arr, column4name, column4index):
+        for row in arr:
+            if row[column4name] == itemName:
+                return row[column4index]
     
     def getCardRank(self, card_name, game_type = '4player_default'):
         workbook_list = self.workbook_list
         
-        arr_rank = getValuesFromSheet(workbook_list[game_type])
-        card_rank = getItemIndexByNameFromArray(card_name, arr_rank, 1, 0)
+        arr_rank = self.getValuesFromSheet(workbook_list[game_type])
+        card_rank = self.getItemIndexByNameFromArray(card_name, arr_rank, 1, 0)
         return card_rank
 
     def getCardDiff(self, card_name):
         workbook_list = self.workbook_list
 
-        arr_diff = getValuesFromSheet(workbook_list['Diff'])
-        card_diff = getItemIndexByNameFromArray(card_name, arr_diff, 0, 3)
+        arr_diff = self.getValuesFromSheet(workbook_list['Diff'])
+        card_diff = self.getItemIndexByNameFromArray(card_name, arr_diff, 0, 3)
         return card_diff
 
 
