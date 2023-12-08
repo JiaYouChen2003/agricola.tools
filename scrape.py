675016af41fcd70cd721e0eaab39aee44aeba555
JiaYouChen2003 <justin041510@gmail.com>
2023-12-08 01:30:33 +0100

    :sparkles:Now scrape.py will output the card list through its function

--- scrape.py
+++ scrape.py
@@ -5,20 +5,22 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 url_en_front = 'https://en.'
+url = ''
 
-url = input('Enter the URL:')
-url_en_backstartnum = url.find('boardgamearena.com')
-url_en = url_en_front + url[url_en_backstartnum:]
+def getCardListFromBGA():
+    if url == '':
+        url = input('Enter the URL:')
+        url_en_backstartnum = url.find('boardgamearena.com')
+        url_en = url_en_front + url[url_en_backstartnum:]
 
-chrome_options = Options()
-chrome_options.add_argument("--headless=new")
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
-print('Loading recent website...')
-driver.get(url_en)
+    print('Loading recent data...')
+    driver.get(url_en)
 
-card_board = driver.find_element(By.ID, 'player-boards')
-card_list = card_board.find_elements(By.CLASS_NAME, 'card-title')
+    card_board = driver.find_element(By.ID, 'player-boards')
+    card_list = card_board.find_elements(By.CLASS_NAME, 'card-title')
 
-for card in card_list:
-    print(card.text)
+    return card_list
