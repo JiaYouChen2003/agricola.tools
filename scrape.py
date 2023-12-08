9e63adbbdaa96beec5eb5c359cd539a14388042e
JiaYouChen2003 <justin041510@gmail.com>
2023-12-08 01:03:48 +0100

    :sparkles:Add function: user can now input URL through terminal

--- scrape.py
+++ scrape.py
@@ -3,12 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService 
 from webdriver_manager.chrome import ChromeDriverManager 
 
-url = 'https://en.boardgamearena.com/4/agricola?table=447653098'
+url_en_front = 'https://en.'
+
+url = input('Enter the URL:')
+url_en_backstartnum = url.find('boardgamearena.com')
+url_en = url_en_front + url[url_en_backstartnum:]
 
 options = webdriver.ChromeOptions()
 options.headless = True
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-driver.get(url)
+driver.get(url_en)
 
 card_board = driver.find_element(By.ID, 'player-boards')
 card_list = card_board.find_elements(By.CLASS_NAME, 'card-title')
