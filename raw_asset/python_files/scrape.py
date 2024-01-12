78cc079dd08ad02fb0ce511d2269df2656b7b6d9
JiaYouChen2003 <justin041510@gmail.com>
2024-01-12 01:04:23 +0100

    :white_check_mark: Add more test in inquiry_test.py

--- raw_asset/python_files/scrape.py
+++ raw_asset/python_files/scrape.py
@@ -20,10 +20,7 @@ class ScrapeMachine():
         
         # selenium webdriver setting
         chrome_options = Options()
-        chrome_options.add_argument("--headless=new")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=chrome_options)
     
     def getCardListFromBGA(self, url='', username='', password='', save_login_info=True):
