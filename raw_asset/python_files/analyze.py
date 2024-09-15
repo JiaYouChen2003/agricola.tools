64b4544fa2712461db0d1206e64e1096108c30f1
JiaYouChen2003 <justin041510@gmail.com>
2024-09-15 23:46:39 +0800

    :sparkles: Add more accurate score policy

--- raw_asset/python_files/analyze.py
+++ raw_asset/python_files/analyze.py
@@ -3,6 +3,17 @@ import csv
 from raw_asset.python_files import const_agricolatools
 
 
+# Hand ... Card score
+DO_NOT_WANT_ANOTHER_HARD_COMMIT_SCORE = -50
+HAVE_RESOURCES_FOR_SCORE = 2
+DO_NOT_HAVE_RESOURCES_FOR_SCORE = -3
+WANT_EARLY_FLEX_SCORE = 4
+WANT_RESOURCES_FROM_SCORE = 5
+HAVE_SAME_ACTION_SPACE_AS_SCORE = 15
+HAVE_SAME_ROOM_STRATEGY_AS_SCORE = 7
+HAVE_NOT_SAME_ROOM_STRATEGY_AS_SCORE = -7
+
+
 class AnalyzeMachine():
     def __init__(self):
         self.synergy_reference = []
@@ -31,22 +42,81 @@ class AnalyzeMachine():
                 hand_label = [[] for _ in range(len(card_label))]
             
             for i in range(len(card_label)):
-                if card_label[i] != ['0']:
-                    hand_label[i].extend(card_label[i])
+                hand_label[i].append(card_label[i])
         
         return hand_label
     
     def __getCardLabel(self, card_info):
         '''
-        return conditions, resources, action space, room
+        return ['early', 'mid', 'late', 'flexible', 'conditional', 'hard commit', 'resources', 'action space', 'room', 'early flex']
         
         rtype: list(list(str)) | None
         '''
         # ['Card Name', 'early', 'mid', 'late', 'flexible', 'conditional', 'hard commit', 'resources', 'action space', 'room']
         for row in self.synergy_reference:
             if row[0] == card_info[0]:
-                return [row[5].split(), row[7].split(), row[8].split(), row[9].split()]
+                row = row[1:]
+                card_label = [item.split() for item in row]
+                return card_label
         return None
+    
+    def __getPairSynergyScore(self, card_label, hand_label):
+        early, mid, late, flexible, conditions, hard_commit, resources, action_space, room = card_label
+        hand_early, hand_mid, hand_late, hand_flexible, hand_conditions, hand_hard_commit, hand_resources, hand_action_space, hand_room = hand_label
+        
+        card_synergy_score = 0
+        
+        # hand have resources for card, card is better
+        if conditions != ['0']:
+            for hand_card_resources in hand_resources:
+                for item in hand_card_resources:
+                    if item in conditions:
+                        card_synergy_score += HAVE_RESOURCES_FOR_SCORE
+        
+        # hand do not have resources for not flexible card that need resources, card is worse
+        if card_synergy_score == 0 and flexible == ['0'] and conditions != ['0']:
+            card_synergy_score += DO_NOT_HAVE_RESOURCES_FOR_SCORE
+        
+        # hard commit in hand, do not want another hard commit, card is worse
+        if hard_commit == ['1']:
+            for hand_card_hard_commit in hand_hard_commit:
+                if hand_card_hard_commit == ['1']:
+                    card_synergy_score += DO_NOT_WANT_ANOTHER_HARD_COMMIT_SCORE
+        
+        # hand do not have early flex, want early flex card, card is better
+        if early == ['1'] and flexible == ['1']:
+            want_early_flex = True
+            for hand_card_early, hand_card_flexible in zip(hand_early, hand_flexible):
+                if hand_card_early == ['1'] and hand_card_flexible == ['1']:
+                    want_early_flex = False
+                    break
+            if want_early_flex:
+                card_synergy_score += WANT_EARLY_FLEX_SCORE
+        
+        # hand want resources from card, card is better
+        if resources != ['0']:
+            for hand_card_conditions in hand_conditions:
+                for item in hand_card_conditions:
+                    if item in resources:
+                        card_synergy_score += WANT_RESOURCES_FROM_SCORE
+        
+        # hand have the same action space with card, card is better
+        if action_space != ['0']:
+            for hand_card_action_space in hand_action_space:
+                for item in hand_card_action_space:
+                    if item in action_space:
+                        card_synergy_score += HAVE_SAME_ACTION_SPACE_AS_SCORE
+        
+        # hand have same room strategy with card, card is better
+        # hand have different room strategy with card, card is worse
+        if room != ['0']:
+            for hand_card_room in hand_room:
+                for item in hand_card_room:
+                    if item in room:
+                        card_synergy_score += HAVE_SAME_ROOM_STRATEGY_AS_SCORE
+                    elif room != ['0']:
+                        card_synergy_score += HAVE_NOT_SAME_ROOM_STRATEGY_AS_SCORE
+        return card_synergy_score
     
     def getCardRankMean(self, card_info_arr, player_num=0):
         '''
@@ -74,6 +144,11 @@ class AnalyzeMachine():
     def getCardSynergyScore(self, card_info_arr):
         hand_label = None
         for i, card_info in enumerate(card_info_arr):
+            card_basic_score = 0
+            if card_info_arr[i][1] is not None:
+                card_basic_score += round((100 - 100 * (float(card_info_arr[i][1]) / 526)), 2)
+            card_info_arr[i].insert(-1, str(card_basic_score))
+            
             if card_info[0] == const_agricolatools.CARD_HAND_LABEL:
                 hand_label = self.__getHandLabel(hand_card_info_arr=card_info_arr[i:])
                 break
@@ -81,7 +156,6 @@ class AnalyzeMachine():
         if hand_label is None:
             return card_info_arr
         
-        hand_conditions, hand_resources, hand_action_space, hand_room = hand_label
         for i, card_info in enumerate(card_info_arr):
             if card_info[0] == const_agricolatools.CARD_HAND_LABEL:
                 break
@@ -90,25 +164,6 @@ class AnalyzeMachine():
             if card_label is None:
                 continue
             
-            card_synergy_score = 0
-            card_conditions, card_resources, card_action_space, card_room = card_label
-            
-            for item in hand_conditions:
-                if item in card_resources:
-                    card_synergy_score += 1
-            
-            for item in hand_resources:
-                if item in card_conditions:
-                    card_synergy_score += 1
-            
-            for item in hand_action_space:
-                if item in card_action_space:
-                    card_synergy_score += 1
-            
-            for item in hand_room:
-                if item in card_room:
-                    card_synergy_score += 1
-            
-            card_info_arr[i].append(str(card_synergy_score))
-            card_info_arr[i][-1], card_info_arr[i][-2] = card_info_arr[i][-2], card_info_arr[i][-1]
+            card_synergy_score = self.__getPairSynergyScore(card_label, hand_label)
+            card_info_arr[i][-2] = str(float(card_info_arr[i][-2]) + card_synergy_score)
         return card_info_arr
--- test/analyze_test.py
+++ test/analyze_test.py
@@ -27,6 +27,7 @@ CARD_INFO_ARR_HAND_TESTING0 = [
     ['zz_TEST01', 0, 0, 0],
     ['zz_TEST02', 0, 0, 0],
     ['zz_TEST03', 0, 0, 0],
+    ['zz_TEST04', 0, 0, 0],
     [const_agricolatools.CARD_HAND_LABEL, None, None, 0],
     ['zz_TEST0', 0, 0, 0]]
 
@@ -35,9 +36,17 @@ CARD_INFO_ARR_HAND_TESTING1 = [
     ['zz_TEST12', 0, 0, 0],
     ['zz_TEST13', 0, 0, 0],
     ['zz_TEST14', 0, 0, 0],
-    ['', 0, 0, 0],
+    ['zz_TEST15', 0, 0, 0],
+    ['zz_TEST16', 0, 0, 0],
     [const_agricolatools.CARD_HAND_LABEL, None, None, 0],
     ['zz_TEST1', 0, 0, 0]]
+
+CARD_INFO_ARR_HAND_TESTING2 = [
+    ['zz_TEST21', 0, 0, 0],
+    ['zz_TEST21', None, None, 0],
+    ['TEST_NOT_EXIST', None, None, 0],
+    [const_agricolatools.CARD_HAND_LABEL, None, None, 0],
+    ['zz_TEST2', 0, 0, 0]]
 
 
 @pytest.mark.analyze
@@ -95,9 +104,10 @@ def test_getCardSynergyScore_TEST0():
     
     assert len(card_info_arr) == len(CARD_INFO_ARR_HAND_TESTING0)
     assert len(card_info_arr[0]) == 5
-    assert card_info_arr[0][-2] == '1'
-    assert card_info_arr[1][-2] == '1'
-    assert card_info_arr[2][-2] == '1'
+    assert float(card_info_arr[0][-2]) == 100 + analyze.HAVE_RESOURCES_FOR_SCORE
+    assert float(card_info_arr[1][-2]) == 100 + analyze.DO_NOT_HAVE_RESOURCES_FOR_SCORE
+    assert float(card_info_arr[2][-2]) == 100 + analyze.DO_NOT_WANT_ANOTHER_HARD_COMMIT_SCORE
+    assert float(card_info_arr[3][-2]) == 100
 
 
 @pytest.mark.analyze
@@ -107,7 +117,21 @@ def test_getCardSynergyScore_TEST1():
     
     assert len(card_info_arr) == len(CARD_INFO_ARR_HAND_TESTING1)
     assert len(card_info_arr[0]) == 5
-    assert card_info_arr[0][-2] == '1'
-    assert card_info_arr[1][-2] == '1'
-    assert card_info_arr[2][-2] == '1'
-    assert card_info_arr[3][-2] == '1'
+    assert float(card_info_arr[0][-2]) == 100 + analyze.WANT_EARLY_FLEX_SCORE
+    assert float(card_info_arr[1][-2]) == 100 + analyze.WANT_RESOURCES_FROM_SCORE
+    assert float(card_info_arr[2][-2]) == 100 + analyze.HAVE_SAME_ACTION_SPACE_AS_SCORE
+    assert float(card_info_arr[3][-2]) == 100 + analyze.HAVE_SAME_ROOM_STRATEGY_AS_SCORE
+    assert float(card_info_arr[4][-2]) == 100 + analyze.HAVE_NOT_SAME_ROOM_STRATEGY_AS_SCORE
+    assert float(card_info_arr[5][-2]) == 100
+
+
+@pytest.mark.analyze
+def test_getCardSynergyScore_TEST2():
+    machine_analyze = AnalyzeMachine()
+    card_info_arr = machine_analyze.getCardSynergyScore(CARD_INFO_ARR_HAND_TESTING2)
+    
+    assert len(card_info_arr) == len(CARD_INFO_ARR_HAND_TESTING2)
+    assert len(card_info_arr[0]) == 5
+    assert float(card_info_arr[0][-2]) == 100
+    assert float(card_info_arr[1][-2]) == 0
+    assert float(card_info_arr[2][-2]) == 0
